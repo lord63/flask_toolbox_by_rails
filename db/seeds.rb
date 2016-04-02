33afeca4fa59ee7eb5000c089551d360a69ab7b6
lord63 <lord63.j@gmail.com>
2016-04-02 21:05:30 +0800

    Support load info from yaml file, close #3
    running via: `bin/rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'yaml'
+
+
+meta_file = File.join(File.dirname(File.realdirpath(__FILE__)), 'packages.yml')
+file = File.open(meta_file)
+meta_info = YAML.load(file)
+file.close
+
+meta_info["categories"].each_pair do |category_name, category_info|
+  category = Category.create(name: category_name,
+                             description: category_info['description'])
+
+  category_info['packages'].each do |package|
+    package_info = meta_info['packages'][package]
+    Package.create(name: package,
+                   category_id: category.id,
+                   description: package_info['description'],
+                   pypi_url: package_info['pypi_url'],
+                   documentation_url: package_info['pypi_url'],
+                   source_code_url: package_info['pypi_url'],
+                   bug_tracker_url: package_info['pypi_url'])
+  end
+end
