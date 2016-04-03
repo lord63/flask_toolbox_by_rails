a700ffb6dd272f5d4767a9596b71588b10b78b44
lord63 <lord63.j@gmail.com>
2016-04-03 11:00:57 +0800

    We have a basic github parser now

---
+++ lib/tasks/github.rb
@@ -0,0 +1,33 @@
+Module GithubMeta
+  class GithubMeta
+    def initialize(response, url)
+    end
+
+    def watchers
+    end
+
+    def forks
+    end
+
+    def last_commit
+    end
+
+    def contributors
+    end
+
+    def commits
+    end
+
+    def issues
+    end
+
+    def pull_requests
+    end
+  end
+
+  def self.get_first_commit(url, commit_num)
+  end
+
+  def self.get_development_activity(url)
+  end
+end
