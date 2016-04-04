de9a7c26e97ceb26494d179c9c16de6a73615510
lord63 <lord63.j@gmail.com>
2016-04-04 08:44:10 +0800

    Generate the packages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :categories, :only => [:index, :show]
+  resources :packages, :only => [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
