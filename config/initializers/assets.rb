d97ba6e7fd247945d4e2aaa3eb964e22b2a0e825
lord63 <lord63.j@gmail.com>
2016-04-04 10:17:43 +0800

    Mark the precompile assert

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css flask-toolbox.css
+                                                  jquery-1.11.3.min.js bootstrap.min.js)
